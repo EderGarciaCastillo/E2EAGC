f370e5033de8d8e0b9d53dcf75dc5e1422797caa
EderGarciaCastillo <eder.garciacs@uanl.edu.mx>
2020-09-15 00:22:16 -0500

    Script E2EAGC

--- E2EAGCcarpeta/E2EAGC.py
+++ E2EAGCcarpeta/E2EAGC.py
@@ -4,23 +4,56 @@ import sys
 from bs4 import BeautifulSoup as bs
 import webbrowser
 
+
 '''
 Eder Antonio García Castillo 1799409
 
+Este script hace la busqueda en la pagina de noticias de la universidad
+de la facultad solicitada por sus siglas y con un ciclo busca entre el
+rango proporcionado, al encontrar algo abre una pagina web directamente.
+Abre una pestaña por cada noticia encontrada.
 '''
 
-print("Este script navega en las páginas de noticas de la UANL")
+
+try: 
+    import webbrowser 
+except ImportError: 
+    os.system('pip install webbrowser') 
+    print('Installing webbrowser...') 
+    print('Ejecuta de nuevo tu script...') 
+    exit()
+
+try: 
+    import requests 
+except ImportError: 
+    os.system('pip install requests') 
+    print('Installing requests...') 
+    print('Ejecuta de nuevo tu script...') 
+    exit()
+
+try: 
+    from bs4 import BeautifulSoup as bs 
+except ImportError: 
+    os.system('pip install BeautifulSoup') 
+    print('Installing webbrowser...') 
+    print('Ejecuta de nuevo tu script...') 
+    exit()
+
+
+print("Este script navega en las páginas de noticias de la UANL")
 inicioRango = int(input("Pagina inicial para buscar: "))
 finRango = int(input("Pagina final para buscar: "))
 dependencia = input("Ingrese las siglas de la Facultad a buscar: ")
 
+
 if inicioRango > finRango:
     inicioRango,finRango = finRango,inicioRango
+
 
 for i in range (inicioRango,finRango,1):
     url = "https://www.uanl.mx/noticias/page/"+str(i)
     pagina = requests.get (url)
-    if pagina.status_code != 200:
+    if pagina.status_code != 200: #el codigo 200 es que cargó bien la pagina
         raise TypeError("Pagina no encontrada")
     else:
         soup = bs(pagina.content,"html.parser")
